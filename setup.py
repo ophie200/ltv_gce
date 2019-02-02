70984946ec2426d782c6cf22644cd4e2a76ce493
ophie200 <ophie200@users.noreply.github.com>
2019-02-02 14:14:21 -0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(name='ltv-gce',
+      version='0.1',
+      description='Scripts for running LTV',
+      long_description=readme(),
+      keywords='lifetime ltv clv gcp',
+      url='https://github.com/ophie200/ltv_gce',
+      author='Nancy Wong',
+      author_email='nawong@mozilla.com',
+      license='...',
+      packages=['SurveyGizmo'],
+      install_requires=[
+          'Lifetimes',
+          'dill',
+          'numpy',
+          'pandas',
+          'scipy',
+          'google-cloud-logging',
+          'google-cloud-storage'
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      entry_points={
+          'console_scripts': ['run-ltv-gce=LTV-GCE.ltv_calc:main'],
+      },
+      include_package_data=True,
+zip_safe=False)
